da684b18d096e9c463bc09bbc12407b97df95d5f
fname lname <monzurulislam997@gmail.com>
2022-04-07 16:41:02 +0600

    save error handling done

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 //get all input value function
 function getInputValue(inputId){
     const inputText =document.getElementById(inputId).value;
-    // error handle
+ 
       
     const inputValue =parseFloat(inputText);
-    if(inputValue<=0){
-      return console.log(inputValue,"h")
-    }else
-    {
-      return console.log(inputValue)
-    }
+   return inputValue
+    
 }
 //get all changeable innertext fucntion
     function showAmount (textId){
@@ -24,15 +20,57 @@ document.getElementById('calculate-btn').addEventListener('click',function(){
    const foodCost =getInputValue('foodCost-input');
    const rentCost =getInputValue('rentCost-input');
    const clothesCost =getInputValue('clothesCost-input');
-    const totalCost =foodCost+rentCost+clothesCost;
-   const afterCostBalance =incomeAmount-(foodCost+rentCost+clothesCost);
+   const totalCost =foodCost+rentCost+clothesCost;
+    
+    const errorMessage=document.getElementById("error-message");
+    const errorMessage2=document.getElementById("error-message2");
+    const errorMessage3=document.getElementById("error-message3");
+
+      //error handling
+     if(incomeAmount<0  || foodCost<0  || rentCost<0  || clothesCost<0 ){
+        
+      errorMessage.style.display="block";
+      errorMessage2.style.display="none";
+      console.log("shows 2")
+      
+      showAmount('total-expenses').innerText="00 ";
+     showAmount('afterCost-balance').innerText="00";  
+ 
+     } 
+     else if(isNaN(incomeAmount) || isNaN(foodCost) || isNaN(rentCost) ||isNaN(clothesCost) ){
+      errorMessage2.style.display="block";
+      errorMessage.style.display="none";
+      showAmount('total-expenses').innerText="00";
+     showAmount('afterCost-balance').innerText="00"; 
+      console.log("shows1")
+    } 
+
+
+      else if(totalCost>incomeAmount){
+        errorMessage3.style.display="block";
+        showAmount('total-expenses').innerText="00";
+     showAmount('afterCost-balance').innerText="00"; 
+     console.log('Shows 3')
+      }
+
+    else {
+      errorMessage.style.display="none";
+      errorMessage2.style.display="none"
+      errorMessage3.style.display="none"
+      showAmount('total-expenses').innerText="00";
+      showAmount('afterCost-balance').innerText="00"; 
+        const afterCostBalance =incomeAmount-(foodCost+rentCost+clothesCost);
     
     showAmount('total-expenses').innerText=totalCost;
      showAmount('afterCost-balance').innerText=afterCostBalance;  
-   
+
+    }
 
 })
 
+
+
+//error handling end
 
 //save update
 document.getElementById('save-btn').addEventListener('click',function(){
@@ -40,7 +78,11 @@ document.getElementById('save-btn').addEventListener('click',function(){
  //save update
  const incomeAmount = getInputValue('income-input');
  const savePercentage =  getInputValue("savePercentage-input");
- const savingAmount =incomeAmount*(savePercentage/100);
+ 
+  if(savePercentage<0){
+    document.getElementById("save-error-message").style.display="block"
+  } else {
+    const savingAmount =incomeAmount*(savePercentage/100);
     document.getElementById("saving-amount").innerText=savingAmount;
    const afterCostBalanceText =showAmount("afterCost-balance").innerText;
   
@@ -50,6 +92,9 @@ document.getElementById('save-btn').addEventListener('click',function(){
   
   document.getElementById("remaining-amount").innerText=remainingAmount;
 
+  
+
+  }
   
  
 
